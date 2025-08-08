b524c57a9008053d4828d9fed80f63181d6a19a7
choneface <gavin.garcia0001@gmail.com>
2025-08-08 06:30:47 -0400

    add media query to check screensize and fallback to splash screen

---
+++ src/components/SmallScreenFallback.tsx
@@ -0,0 +1,15 @@
+import "./SmallScreenFallback.css";
+
+export default function SmallScreenFallback() {
+  return (
+    <div className="small-screen-fallback">
+      <div>
+        <h1>Bigger Screen, Bigger Adventure</h1>
+        <p>
+          Our terminal-style journey is built for laptop & desktop displays.
+          Join us on a full screen to begin your story.
+        </p>
+      </div>
+    </div>
+  );
+}
